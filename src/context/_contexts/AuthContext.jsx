dcce1015a75bcb88f7c543c9d82f4cc9c8fff9f7
zacheys123 <105823151+zacheys123@users.noreply.github.com>
2024-01-18 00:03:08 +0300

    Update AuthContext.jsx

--- src/context/_contexts/AuthContext.jsx
+++ src/context/_contexts/AuthContext.jsx
@@ -1,25 +1,24 @@
-import React, { useContext, useReducer } from 'react'
-import { AuthProvider } from '../config'
-import auth_reducer from '../reducers/auth'
-import {auth_state } from '../reducers/auth/auth_state'
-const AuthContext = ({children}) => {
+import React, { useContext, useReducer } from 'react';
+import { AuthProvider } from '../config';
+import { auth, auth_state } from '../reducers/auth_reducer';
+const AuthContext = ({ children }) => {
+	const [auth_state, setAuth] = useReducer(auth, auth_state);
 
-    const[auth,setAuth]=useReducer(auth_reducer,auth_state)
+	let value = { auth_state, setAuth };
+	return (
+		<AuthProvider.Provider value={value}>
+			{children}
+		</AuthProvider.Provider>
+	);
+};
 
-    let value={auth,setAuth}
-      return (
-    <AuthProvider.Provider value={value}>
-      {children}
-    </AuthProvider.Provider>
-  )
-}
-
-
-export  const useAuthContext=()=>{
-const context=useContext(AuthProvider)
-if(!context){
-    throw new Error('useAuthContext must be used within AuthContextProvider')
-}
-    return context
-}
-export default AuthContext
+export const useAuthContext = () => {
+	const context = useContext(AuthProvider);
+	if (!context) {
+		throw new Error(
+			'useAuthContext must be used within AuthContextProvider',
+		);
+	}
+	return context;
+};
+export default AuthContext;
