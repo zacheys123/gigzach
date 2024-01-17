4ec5c62d6ddb09fc96c8a08435366290463f3ca9
zacheys123 <105823151+zacheys123@users.noreply.github.com>
2024-01-17 23:57:11 +0300

    Update index.js

--- src/context/reducers/auth/index.js
+++ src/context/reducers/auth/index.js
@@ -1,12 +1,1 @@
-import { THEME } from "../../actions"
-import  {auth_state} from './auth_state'
 
-const  index=(action,state={auth_state})=>{
-    switch(action.type){
-        case THEME:
-            return {...state,theme:!action.payload}
-        default:
-            return state
-    }
-}
-export default index
