ec4578c6b5ea1a4e5e706a0b1787ed7e716c511a
zacheys123 <105823151+zacheys123@users.noreply.github.com>
2024-01-22 22:34:29 +0000

    refactor auth

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -14,6 +14,12 @@ const Register = () => {
     authstate:{registerform},
     setAuth
   }=useAuthContext()
+
+  console.log(Array(10).fill(0,10))
+  const[f_page,setF]=useState(false)
+  const[s_page,setS]=useState(false)
+  const[t_page,setT]=useState(true)
+  const[q_page,setQ]=useState(false)
   const handleSubmit = (ev) => {
     ev.preventDefault();
     alert("form submitted");
@@ -36,12 +42,15 @@ const Register = () => {
   const handleInput=(ev)=>{
     setData({...form_data,[ev.target.name]:ev.target.value})
   }
+  constmyArray=["January","February","March","April","May","June","July","August","September","October","November","December"]
  return (
     <Box className="form-container">
     <form action="" onSubmit={handleSubmit}>
       <motion.div initial=
       {{x:'300px',opacity:0}} animate={{x:[0,-125,0],opacity:[1,0,0,0,1]}}>
 <h5 style={{color:'white',fontFamily:'monospace'}}>Personal info</h5>
+{f_page &&
+<>
 <input
       type="text"
       name="firstname"
@@ -73,10 +82,135 @@ const Register = () => {
       onChange={handleInput}
       placeholder="Email Address"
     />
+
+<div style={{textAlign:'right',margin:'.8rem auto'}}>
+      <Button variant="contained" sx={{fontFamily:'monospace'}}>Next</Button>
+      </div>
+    </>
+
+
+} 
+  {/*load the second page*  */}
+{s_page &&
+  <motion.div>
+<input
+      type="text"
+      name="firstname"
+      value={form_data.firstname}
+      onChange={handleInput}
+      placeholder="Enter Firstname"
+    />
+
+    <input
+      type="text"
+      name="second"
+      value={form_data.tel}
+     onChange={handleInput}
+      placeholder="Tel No...."
+   
+    />
+
+    <input
+      type="text"
+      name="city"
+      value={form_data.country}
+      onChange={handleInput}
+      placeholder="Country"
+    />
+        <input
+      type="text"
+      name="email"
+      value={form_data.city}
+      onChange={handleInput}
+      placeholder="City"
+    />
+  
+
     <div style={{textAlign:'right',margin:'.8rem auto'}}>
       <Button variant="contained" sx={{fontFamily:'monospace'}}>Next</Button>
       </div>
       </motion.div>
+}
+{/* load the third page */}
+{t_page &&
+<motion.div>
+<input
+      type="text"
+      name="second"
+      value={form_data.second}
+     onChange={handleInput}
+      placeholder="Enter Second Name"
+   
+    />
+    <select name='day' value={form_data.day} onChange={handleInput}>
+      {
+Array(31).fill().map((_, idx) => 
+  <option value={idx + 1}  key={idx}>{idx + 1}</option>
+)
+      }
+    </select>
+
+    <select name='day' value={form_data.day} onChange={handleInput}>
+      {
+myArray.map((_month, idx) => 
+  <option value={form_data.month}  key={idx}>{_month}</option>
+)
+      }
+    </select>
+        <input
+      type="text"
+      name="email"
+      value={form_data.email}
+      onChange={handleInput}
+      placeholder="Email Address"
+    />
+  
+
+    <div style={{textAlign:'right',margin:'.8rem auto'}}>
+      <Button variant="contained" sx={{fontFamily:'monospace'}}>Next</Button>
+      </div>
+
+</motion.div>
+}
+{/* load the fourth page */}
+{q_page &&
+<motion.div>
+<input
+      type="text"
+      name="second"
+      value={form_data.second}
+     onChange={handleInput}
+      placeholder="Enter Second Name"
+   
+    />
+
+    <input
+      type="text"
+      name="username"
+      value={form_data.username}
+      onChange={handleInput}
+      placeholder="Username"
+    />
+        <input
+      type="text"
+      name="email"
+      value={form_data.email}
+      onChange={handleInput}
+      placeholder="Email Address"
+    />
+  
+
+    <div style={{textAlign:'right',margin:'.8rem auto'}}>
+      <Button variant="contained" sx={{fontFamily:'monospace'}}>Next</Button>
+      </div>
+
+</motion.div>
+}
+
+
+
+      </motion.div>
+
   
     </form> 
     </Box>
